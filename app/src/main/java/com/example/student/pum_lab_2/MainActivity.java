16b1bd9e1af9fbbc0f81d440f942ec505e85899b
mateceb <matekcebrat@gmail.com>
2017-12-17 02:14:11 +0100

    Revert "Zadanie_12"
    This reverts commit d992fbd6404b075a829cc85f6e8fda6ec996b64c.

--- app/src/main/java/com/example/student/pum_lab_2/MainActivity.java
+++ app/src/main/java/com/example/student/pum_lab_2/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.student.pum_lab_2;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -18,10 +17,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button przycisk_zmien = findViewById(R.id.przycisk_zmien);
-        Button przycisk_lista = findViewById(R.id.przycisk_lista);
 
 
-          }
+    }
 
     public void clik_zmien_miejsce(View view)
     {
@@ -33,18 +31,9 @@ public class MainActivity extends AppCompatActivity {
 
     public  void  clik_lista( View view)
     {
-        Intent intent = new Intent(MainActivity.this, Activity2.class);
-        intent.putExtra("miasto", R.id.miasto);
-        Bundle bundle = new Bundle();
-        bundle.putString("miasto", String.valueOf(R.id.podaj_nazwe));
-        intent.putExtras(bundle);
-
-
+        Intent intent = new Intent(getApplicationContext(), Activity2.class);
         startActivity(intent);
     }
-
-
-
 
 
 
